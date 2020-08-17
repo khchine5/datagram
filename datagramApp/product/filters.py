e33fa083e99cfc7fb3f3ccf745a161a512ce9068
Hamza Khchine <hamzakhchine@gmail.com>
2020-08-17 11:18:22 +0100

    Add filter by store and prices

--- datagramApp/product/filters.py
+++ datagramApp/product/filters.py
@@ -5,3 +5,8 @@ import django_filters
 class NameFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_expr='icontains')
     active = django_filters.BooleanFilter()
+    min_price = django_filters.NumberFilter(field_name="price", lookup_expr="gte")
+    max_price = django_filters.NumberFilter(field_name="price", lookup_expr="lte")
+    stores = django_filters.ModelChoiceFilter(queryset=Store.objects.order_by('name'))
+
+
