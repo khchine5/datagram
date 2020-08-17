4a2e9b409aa1a4f4259dfe711521c1621024c555
Hamza Khchine <hamzakhchine@gmail.com>
2020-08-17 11:25:46 +0100

    Add DATABASES prod

--- datagramApp/datagramApp/settings/prod.py
+++ datagramApp/datagramApp/settings/prod.py
@@ -3,3 +3,10 @@ from .base import *
 DEBUG = True
 ALLOWED_HOSTS = ['khchine5.pythonanywhere.com']
 CORS_ORIGIN_ALLOW_ALL = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'dbprod.sqlite3',
+    }
+}
